3b6c632f1b4edca1330e05d964812080810fde3f
Etienne <e.sharkey@live.com>
2023-04-26 02:01:17 +0100

    Simple day overview component

---
+++ src/components/DayOverview/DayOverview.js
@@ -0,0 +1,13 @@
+
+
+function DayOverview (props) {
+
+
+  return (
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+    </li>
+  )
+}
+
+export default DayOverview;
