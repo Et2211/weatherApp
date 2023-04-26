6b215faaed7fb508dcee46cbc7fb3636542cec52
Etienne <e.sharkey@live.com>
2023-04-26 02:20:16 +0100

    pass day to day overview. set day onclick

--- src/components/DayOverview/DayOverview.js
+++ src/components/DayOverview/DayOverview.js
@@ -3,10 +3,13 @@
 function DayOverview (props) {
 
 
+
   return (
-    <li class="nav-item" role="presentation">
-      <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+    <>
+    <li class="nav-item" role="presentation" onClick={()=>(props.setSelectedDay(props.day))}>
+      <button class={props.selectedDay == props.day ? "nav-link active" : "nav-link"} id="home-tab" data-bs-toggle="tab" data-bs-target={"#day"+props.day} type="button" role="tab" aria-controls="home" aria-selected={props.selectedDay == props.day}>Home</button>
     </li>
+    </>
   )
 }
 
--- src/components/DaySelect/DaySelect.js
+++ src/components/DaySelect/DaySelect.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import DayOverview from "../DayOverview/DayOverview";
 import styles from './DaySelect.module.scss'
 function DaySelect(props) {
 
+  const [selectedDay, setSelectedDay] = useState(0)
+
   const addDaysToToday = (daysToadd) => {
     return new Date(new Date().setDate(new Date().getDate() + daysToadd)); 
   }
-
 
   const checkIfDatesMatch = (weatherDate, dateTocheck) => {
 
@@ -17,18 +19,20 @@ function DaySelect(props) {
   return (
     <div >
       <ul class={"nav nav-tabs "+ styles.tabs} id="myTab" role="tablist">
-        <DayOverview day={0} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(0)))} />
-        <DayOverview day={1} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(1)))} />
-        <DayOverview day={2} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(2)))} />
-        <DayOverview day={3} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(3)))} />
-        <DayOverview day={4} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(4)))} />
+        <DayOverview day={0} selectedDay={selectedDay} setSelectedDay={setSelectedDay} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(0)))} />
+        <DayOverview day={1} selectedDay={selectedDay} setSelectedDay={setSelectedDay} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(1)))} />
+        <DayOverview day={2} selectedDay={selectedDay} setSelectedDay={setSelectedDay} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(2)))} />
+        <DayOverview day={3} selectedDay={selectedDay} setSelectedDay={setSelectedDay} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(3)))} />
+        <DayOverview day={4} selectedDay={selectedDay} setSelectedDay={setSelectedDay} weather={props.weather.list.filter(weatherEvent=>checkIfDatesMatch(new Date(weatherEvent.dt*1000), addDaysToToday(4)))} />
 
       </ul>
 
       <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+        <div class="tab-pane fade show active" id="day0" role="tabpanel" aria-labelledby="home-tab">1</div>
+        <div class="tab-pane fade" id="day1" role="tabpanel" aria-labelledby="profile-tab">2</div>
+        <div class="tab-pane fade" id="day2" role="tabpanel" aria-labelledby="contact-tab">3</div>
+        <div class="tab-pane fade" id="day3" role="tabpanel" aria-labelledby="contact-tab">4</div>
+        <div class="tab-pane fade" id="day4" role="tabpanel" aria-labelledby="contact-tab">5</div>
       </div>
     </div>
   );
