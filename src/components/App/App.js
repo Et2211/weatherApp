a82930c7f652f9391add854928e6cc7dd57adc00
Etienne <e.sharkey@live.com>
2023-04-26 02:04:37 +0100

    Render day select on weather state load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,26 @@
 import styles from './App.module.scss';
+import { useState } from 'react';
 import { connect, useSelector, useDispatch } from 'react-redux';
-import { fetchWeather } from '../../redux/actions/weatherActions';
+import { fetchCities, fetchWeather } from '../../redux/actions/weatherActions';
 import store from '../../store'
 import { getRequest } from '../../api';
 import { useEffect } from 'react';
 import Header from '../Header/Header';
+import DaySelect from '../DaySelect/DaySelect';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
 
   const dispatch = useDispatch()
+  const cityId = useState(1)
+
+  const locations = useSelector(state=>state.locationsReducer.locations) 
+  const weather = useSelector(state=>state.weatherReducer.weather) 
 
   useEffect(()=>{
-    refreshLocation()
+    getLocations()
+    getWeather(cityId)
   }, [])
 
   const getLocations = () => {
@@ -26,10 +35,14 @@ function App(props) {
     })
   }
 
-
   return (
     <div className={styles.App}>
       <Header />
+
+
+      {weather && Object.keys(weather).length > 0 &&
+        <DaySelect locations={locations} weather={weather}/>
+      }
     </div>
   );
 }
