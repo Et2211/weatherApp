d3a0d4978d8068f737a677f35810455d1517f7bf
Etienne <e.sharkey@live.com>
2023-04-27 17:14:14 +0100

    Write tests for snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './components/App/App'
+import CitySelector from './components/CitySelector/CitySelector'
+import { Provider } from 'react-redux'
+import configureStore from './store';
+import renderer from 'react-test-renderer'; // ES6
+import './setupTests'
+const store = configureStore()
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('./components/CitySelector/CitySelector')
+CitySelector.mockImplementation(props => props.user)
+jest.mock('./components/DayOverview/DayOverview')
+
+test('1. App renders', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>   
+  )
+  
+  expect(screen.getByText(/Wagonex weather/i)).toBeInTheDocument()
+})
+
+test('2. App matches snapshot', () => {
+  const appTemplate = renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>   
+  )
+  expect(appTemplate).toMatchSnapshot();
+
+})
